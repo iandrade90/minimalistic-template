9a242c94d818326aa666ebe943e7992c5ff72326
Ivan Andrade <76601799+iandrade90@users.noreply.github.com>
2021-07-21 23:53:37 +0000

    Counter

--- Js/index.js
+++ Js/index.js
@@ -6,3 +6,25 @@ window.addEventListener('scroll', function(){
   let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0);
 })
+
+//COUNTER
+
+const counters = document.querySelectorAll('.value');
+const speed = 20000;
+
+counters.forEach( counter => {
+  const animate = () => {
+    const value = +counter.getAttribute('akhi');
+    const data = +counter.innerText;
+
+    const time = value / speed;
+    if(data < value) {
+      counter.innerText = Math.ceil(data + time);
+      setTimeout(animate, 1); 
+    } else {
+      counter.innerText = value;
+    }
+  }
+
+  animate();
+});
