d86f0949802922c08c33fef7573a66cccefd6e95
Ivan Andrade <ivandandrade90@gmail.com>
2021-08-04 01:58:18 -0300

    Update ago 4th

--- Js/index.js
+++ Js/index.js
@@ -1,30 +1,119 @@
 //FIXED NAVBAR
 
-console.log('hola');
-
 window.addEventListener('scroll', function(){
   let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0);
 })
 
-//COUNTER
+//SCROLLBTN
 
-const counters = document.querySelectorAll('.value');
-const speed = 20000;
+document.addEventListener('scroll', handleScroll);
 
-counters.forEach( counter => {
-  const animate = () => {
-    const value = +counter.getAttribute('akhi');
-    const data = +counter.innerText;
+const btn = document.querySelector('.btn');
 
-    const time = value / speed;
-    if(data < value) {
-      counter.innerText = Math.ceil(data + time);
-      setTimeout(animate, 1); 
-    } else {
-      counter.innerText = value;
-    }
+btn.addEventListener('click', function () {
+  window.scrollTo(0, 0);
+})
+
+
+function handleScroll(){
+  let btn = document.querySelector('.btn-container');
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.15;
+
+  if((document.documentElement.scrollTop / scrollableHeight) > GOLDEN_RATIO){
+    btn.style.display = 'block';
+  } else {
+    btn.style.display = 'none';
   }
+}
 
-  animate();
-});
+
+//SEGMENT ONE ANIMATION
+
+document.addEventListener('scroll', handleScrollOne);
+
+function handleScrollOne(){
+  let section = document.querySelector('.segment-one');
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.08;
+
+  if((document.documentElement.scrollTop / scrollableHeight) > GOLDEN_RATIO){
+    section.classList.add('segment-one-show');
+  }
+}
+
+//SEGMENT TWO ANIMATION
+
+document.addEventListener('scroll', handleScrollFirst);
+document.addEventListener('scroll', handleScrollSecond);
+document.addEventListener('scroll', handleScrollThird);
+document.addEventListener('scroll', handleScrollFourth);
+
+function handleScrollFirst(){
+  let section = document.querySelector('.segment-two-item.one');
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.18;
+
+  if((document.documentElement.scrollTop / scrollableHeight) > GOLDEN_RATIO){
+    section.classList.add('show');
+  }
+}
+
+function handleScrollSecond(){
+  let section = document.querySelector('.segment-two-item.two');
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.30;
+
+  if((document.documentElement.scrollTop / scrollableHeight) > GOLDEN_RATIO){
+    section.classList.add('show');
+  }
+}
+
+function handleScrollThird(){
+  let section = document.querySelector('.segment-two-item.three');
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.42;
+
+  if((document.documentElement.scrollTop / scrollableHeight) > GOLDEN_RATIO){
+    section.classList.add('show');
+  }
+}
+
+function handleScrollFourth(){
+  let section = document.querySelector('.segment-two-item.four');
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.52;
+
+  if((document.documentElement.scrollTop / scrollableHeight) > GOLDEN_RATIO){
+    section.classList.add('show');
+  }
+}
+
+//SEGMENT THREE ANIMATION
+
+document.addEventListener('scroll', handleScrollThree);
+
+function handleScrollThree(){
+  let section = document.querySelector('.segment-three');
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.65;
+
+  if((document.documentElement.scrollTop / scrollableHeight) > GOLDEN_RATIO){
+    section.classList.add('show');
+  }
+}
+
+//SEGMENT FOUR ANIMATION
+
+document.addEventListener('scroll', handleScrollfour);
+
+function handleScrollfour(){
+  let section = document.querySelector('.segment-four');
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.75;
+
+  if((document.documentElement.scrollTop / scrollableHeight) > GOLDEN_RATIO){
+    section.classList.add('show');
+  }
+}
